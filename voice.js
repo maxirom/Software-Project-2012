3e11956bb1c1e001de9bb9df7b2b3e1940756d95
s9pimenn <s9pimenn@stud.uni-saarland.de>
2012-06-24 15:20:26 +0200

    Spoken Output improved -changes src of audio tag -does not add and remove audio tag from DOM constantly

--- voice.js
+++ voice.js
@@ -1,10 +1,11 @@
 /**
- * @author Pia
+ * @author Group 2
  */
 
 //defining a custom grammar is not possible yet
 //document.body.innerHTML += '<div id="gVContainer" class="popup" ><input id="googleVoice" type="text" x-webkit-grammar="grammar.grxml" onwebkitspeechchange="onChange(this.value)" x-webkit-speech /></div>';
 
+//this is the div for googleVoice
 var gvContainer = document.createElement('div');
 gvContainer.id = "gvContainer";
 gvContainer.setAttribute('class', 'popup');
@@ -26,16 +27,25 @@ gvContainer.appendChild(closeButton);
 gvContainer.appendChild(googleVoice);
 document.body.appendChild(gvContainer);
 
+//this appends the Spoken Interaction Functionaloty to all img elements in the page
 var images = document.getElementsByTagName('img');
 for(var i = 0; i < images.length; i++) {
     images[i].addEventListener("click", makeVisible, false);
 };
 
+//this should lock the screen during spoken output
 var lockScreen = document.createElement('div');
 lockScreen.id = 'lockScreen';
 document.body.appendChild(lockScreen);
 
+//this is the Spoken Output element
+var spokenOutput = document.createElement('audio');
+document.body.appendChild(spokenOutput);
+spokenOutput.setAttribute('id','spokenOutput');
+
+
 function makeVisible() {
+    gvContainer.value = '';
     gvContainer.style.visibility = 'visible';
 }
 
@@ -68,6 +78,7 @@ function dialogueManager(results, val) {
             }
         };
         negativeOutput(str);
+        alert('negative Output is over')
         makeVisible();
     }
 
@@ -81,68 +92,30 @@ function dialogueManager(results, val) {
 
 function negativeOutput(userInput) {
     
-    
-    //var url = "http://mary.dfki.de:59125/"; //not dfki dependent
-    var url = 'http://134.96.189.12:59125/';
-    //dfki dependent
+    var url = "http://mary.dfki.de:59125/"; //not dfki dependent
+    //var url = 'http://134.96.189.12:59125/'; //dfki dependent
     var output = "I'm%20sorry%2C%20I%20could%20not%20understand%20what%20%22" + userInput + "%22%20is%20supposed%20to%20mean.%20Please%20rephrase%20your%20command."
     var maryString = "process?INPUT_TYPE=TEXT&OUTPUT_TYPE=AUDIO&INPUT_TEXT=" + output + "&OUTPUT_TEXT=&effect_Volume_selected=&effect_Volume_parameters=amount%3A2.0%3B&effect_Volume_default=Default&effect_Volume_help=Help&effect_TractScaler_selected=&effect_TractScaler_parameters=amount%3A1.5%3B&effect_TractScaler_default=Default&effect_TractScaler_help=Help&effect_F0Scale_selected=&effect_F0Scale_parameters=f0Scale%3A2.0%3B&effect_F0Scale_default=Default&effect_F0Scale_help=Help&effect_F0Add_selected=&effect_F0Add_parameters=f0Add%3A50.0%3B&effect_F0Add_default=Default&effect_F0Add_help=Help&effect_Rate_selected=&effect_Rate_parameters=durScale%3A1.5%3B&effect_Rate_default=Default&effect_Rate_help=Help&effect_Robot_selected=&effect_Robot_parameters=amount%3A100.0%3B&effect_Robot_default=Default&effect_Robot_help=Help&effect_Whisper_selected=&effect_Whisper_parameters=amount%3A100.0%3B&effect_Whisper_default=Default&effect_Whisper_help=Help&effect_Stadium_selected=&effect_Stadium_parameters=amount%3A100.0&effect_Stadium_default=Default&effect_Stadium_help=Help&effect_Chorus_selected=&effect_Chorus_parameters=delay1%3A466%3Bamp1%3A0.54%3Bdelay2%3A600%3Bamp2%3A-0.10%3Bdelay3%3A250%3Bamp3%3A0.30&effect_Chorus_default=Default&effect_Chorus_help=Help&effect_FIRFilter_selected=&effect_FIRFilter_parameters=type%3A3%3Bfc1%3A500.0%3Bfc2%3A2000.0&effect_FIRFilter_default=Default&effect_FIRFilter_help=Help&effect_JetPilot_selected=&effect_JetPilot_parameters=&effect_JetPilot_default=Default&effect_JetPilot_help=Help&HELP_TEXT=&exampleTexts=I'm%20Spike.&VOICE_SELECTIONS=dfki-spike%20en_GB%20male%20unitselection%20general&AUDIO_OUT=WAVE_FILE&LOCALE=en_GB&VOICE=dfki-spike&AUDIO=WAVE_FILE";
-
-    var testAudio = document.createElement('audio');
-    var testSource = document.createElement('source');
-    testAudio.appendChild(testSource);
-    testAudio.id = "testAudio";
-    testAudio.autoplay = "autoplay";
-    testSource.src = url + maryString;
-    testAudio.addEventListener('loadstart', lockScreen, false);
-    testAudio.addEventListener('ended', destroyAudio, false);
-    document.body.appendChild(testAudio);
-    //document.body.removeChild(testAudio);
-    /**
-     $.ajax({
+    
+    $.ajax({
      url : url + maryString,
      type : "GET",
      dataType : 'jsonp audio/x-wav',
      statusCode : {
      404 : function() {
-     alert("page not found");
+     alert("I am sorry, I did not understand you! Please rephrase your command!");
      },
      200 : function() {
-     alert("success");
+        spokenOutput.setAttribute('src',url+maryString);
+        spokenOutput.play();
      }
      },
      cache : false,
-     error : function() {
-     resultValue = false;
-     },
-     success : function(data) {
-     console.log('success:', data);
-     //--> balblal
-     resultValue = data;
-     },
-     complete : function(textStatus) {
-     alert("status of completion: " + textStatus)
-     }
      });
-     **/
-    /**
-     $.get(url+maryString, function(data) {
-     $('.result').html(data);
-     alert('Load was performed.');
-     }, 'jsonp audio/x-wav');
-     **/
 }
 
-function destroyAudio() {
-    lockScreen.style.visibility = 'hidden';
-    var audioElements = $('audio');
-    for(var i = 0; i < audioElements.length; i++) {
-        audioElements[i].parentElement.removeChild(audioElements[i]);
-    };
 
-    function lockScreen() {
+function lockScreen() {
         alert('screen will be locked');
         lockScreen.style.visibility = 'visible';
     }
-
-}
